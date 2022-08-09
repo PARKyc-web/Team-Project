ee3213972e64bb056170f72b1975d59af8ffd00d
admin <admin@YD3-10>
2022-08-09 10:52:25 +0900

    update reservation success page

--- team-project/src/main/java/com/dev/controller/ReservationSuccessController.java
+++ team-project/src/main/java/com/dev/controller/ReservationSuccessController.java
@@ -15,7 +15,7 @@ public class ReservationSuccessController implements Controller {
 	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		ReservationService reservationService = ReservationService.getInstance();
 		int result = reservationService.insertReservation(req.getParameter("memberId"), Integer.parseInt(req.getParameter("hotelId")), req.getParameter("checkIn"), req.getParameter("checkOut"), Integer.parseInt(req.getParameter("totalPrice")));
-		
+
 		req.setAttribute("success", result);
 		
 		Utils.forward(req, resp, "reservation/reservationSuccess.tiles");
